bca3aa73198f3629e98b05019c706ec14e5b1dc3
ansqazzafi <ans.qazzafi@bracketsltd.email>
2024-12-18 12:50:22 -0800

    add

--- src/modules/chatbot/chatbot.gateway.ts
+++ src/modules/chatbot/chatbot.gateway.ts
@@ -11,10 +11,9 @@ import { Server, Socket } from 'socket.io';
 import { ChatbotService } from './chatbot.service';
 import { JwtService } from '@nestjs/jwt';
 import { UseGuards } from '@nestjs/common';
-import { VerifyAdminGuard } from 'guards/verify-admin.guard';
-import { UserService } from '../user/user.service';
 import cookie from 'cookie'
-@WebSocketGateway({ cors: { origin: '*' } })
+import { Request } from 'express';
+@WebSocketGateway({   namespace: 'chat',cors: { origin: '*' } })
 export class ChatbotGateway implements OnGatewayConnection, OnGatewayDisconnect {
   @WebSocketServer()
   server: Server;
@@ -22,42 +21,46 @@ export class ChatbotGateway implements OnGatewayConnection, OnGatewayDisconnect 
   constructor(
     private readonly chatbotService: ChatbotService,
     private readonly jwtService: JwtService,
-    private readonly userService: UserService
 
   ) { }
 
   async handleConnection(socket: Socket) {
-    const cookies = socket.handshake.headers.cookie;
-    console.log(cookies, "Cookies")
+  
+    
+    // const cookies = socket.handshake.headers.cookie;
+    // console.log(cookies, "Cookies")
 
-    if (cookies) {
-      // Parse the cookies to extract the access token
-      const parsedCookies = cookie.parse(cookies); // Use cookie.parse() to parse the cookies
-      const accessToken = parsedCookies['access_token']; // Adjust the cookie name if needed
+    // if (cookies) {
+    //   // Parse the cookies to extract the access token
+    //   const parsedCookies = cookie.parse(cookies); // Use cookie.parse() to parse the cookies
+    //   const accessToken = parsedCookies['access_token']; // Adjust the cookie name if needed
 
-      if (!accessToken) {
-        socket.emit('error', 'Authentication access token not found');
-        socket.disconnect();
-        return;
-      }
+    //   if (!accessToken) {
+    //     socket.emit('error', 'Authentication access token not found');
+    //     socket.disconnect();
+    //     return;
+    //   }
 
-      try {
-        // Verify the access token
-        const decodedToken = await this.jwtService.verifyAsync(accessToken, {
-          secret: process.env.ACCESS_KEY, // Ensure the correct secret key is used here
-        });
+    //   try {
+    //     // Verify the access token
+    //     const decodedToken = await this.jwtService.verifyAsync(accessToken, {
+    //       secret: process.env.ACCESS_KEY, // Ensure the correct secret key is used here
+    //     });
 
-        // Attach the user data directly from the decoded token (no need to query the DB)
-        socket.data.user = decodedToken; // Assuming the user data (userId, username, etc.) is in the token
-        console.log('User authenticated:', socket.data.user);
-      } catch (error) {
-        socket.emit('error', 'Invalid or expired access token');
-        socket.disconnect();
-      }
-    } else {
-      socket.emit('error', 'No cookies found in handshake');
-      socket.disconnect();
-    }
+    //     // Attach the user data directly from the decoded token (no need to query the DB)
+    //     socket.data.user = decodedToken; // Assuming the user data (userId, username, etc.) is in the token
+    //     console.log('User authenticated:', socket.data.user);
+    //   } catch (error) {
+    //     socket.emit('error', 'Invalid or expired access token');
+    //     socket.disconnect();
+    //   }
+    // } else {
+    //   socket.emit('error', 'No cookies found in handshake');
+    //   socket.disconnect();
+    // }
+
+    console.log("Connected");
+    
   }
 
 
@@ -69,6 +72,7 @@ export class ChatbotGateway implements OnGatewayConnection, OnGatewayDisconnect 
 
   @SubscribeMessage('startChat')
   async handleStartChat(client: Socket): Promise<WsResponse<any>> {
+console.log("called");
 
     const questions = this.chatbotService.getRandomQuestions(5);
 
